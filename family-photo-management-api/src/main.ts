131836daf379b0bdda68b06af59d0a09c34b36fb
Isabela <isabela.fonseca@dtidigital.com.br>
2025-03-14 10:07:41 -0300

    development: setting prefix, and changing vercel

--- family-photo-management-api/src/main.ts
+++ family-photo-management-api/src/main.ts
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as cors from 'cors';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,8 +9,8 @@ async function bootstrap() {
 
   const allowedOrigins = process.env.ALLOWED_CORS_ORIGINS?.split(',') || [];
 
-  const corsOptions = {
-    origin: (origin, callback) => {
+  app.enableCors({
+    origin: (origin: string, callback: Function) => {
       if (!origin || allowedOrigins.some(allowedOrigin => origin.includes(allowedOrigin))) {
         callback(null, true);
       } else {
@@ -20,11 +19,10 @@ async function bootstrap() {
     },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     preflightContinue: false,
-  };
+  });
 
-  app.use(cors(corsOptions));
   app.useGlobalPipes(new ValidationPipe());
-
+  app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
     .setTitle('Family Photo Management API')
     .setDescription('Family Photo Management API')
@@ -33,7 +31,6 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  app.setGlobalPrefix('api');
 
   SwaggerModule.setup('api/swagger', app, document);
 
